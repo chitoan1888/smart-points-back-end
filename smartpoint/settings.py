09e5eb61edd94078e319cff62c77a77926631b78
chitoankl1888 <tn414288@gmail.com>
2021-11-28 21:24:51 +0700

    make it better

--- smartpoint/settings.py
+++ smartpoint/settings.py
@@ -26,17 +26,13 @@ SECRET_KEY = 'django-insecure-2=h-e%f#@#&r_s*)6ej7ow3^u)+9=17op)n2hkubtu(b49i5oc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-  'https://smart-points-kappa.vercel.app/',
-)
-# CORS_ALLOWED_ORIGINS = [
-#     "https://smart-points-kappa.vercel.app",
-#     "http://localhost:8080",
-#     "http://localhost:3000",
-#     "http://127.0.0.1:9000",
-# ]
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "https://smart-points-kappa.vercel.app",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://127.0.0.1:9000",
+]
 ALLOWED_HOSTS = ['*']
 
 
